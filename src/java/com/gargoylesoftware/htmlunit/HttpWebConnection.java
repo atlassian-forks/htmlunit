82d0508e1195299cd43f7284471b8228e4362e0f
Marcin Gardias <mgardias@atlassian.com>
2026-03-17 12:13:07 +0100

    switching distribution management to Atlassian

--- src/java/com/gargoylesoftware/htmlunit/HttpWebConnection.java
+++ src/java/com/gargoylesoftware/htmlunit/HttpWebConnection.java
@@ -247,8 +247,7 @@ public class HttpWebConnection extends WebConnectionImpl {
                 while (iterator.hasNext()) {
                     final PartBase newPart;
                     final KeyValuePair pair = (KeyValuePair) iterator.next();
-                    if (pair instanceof KeyDataPair) {
-                        final KeyDataPair pairWithFile = (KeyDataPair) pair;
+                    if (pair instanceof KeyDataPair pairWithFile && pairWithFile.getFile().isFile()) {
                         newPart = new FilePart(
                                 pairWithFile.getName(),
                                 pairWithFile.getValue(),
